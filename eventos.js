dce0e66191f5b3646e2487914b5292a9fcb07957
luciaheguy <lucia_heguy@hotmail.com>
2024-08-13 22:00:01 -0300

    Solución del click subida

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
 const divClick = document.getElementById("divClick");
+const button = document.querySelector("#divClick button");
+
 divClick.addEventListener('click', function() {
     alert("Hola! Soy el div");
+});
+
+button.addEventListener('click', function(event){
+    event.stopPropagation();
 });
